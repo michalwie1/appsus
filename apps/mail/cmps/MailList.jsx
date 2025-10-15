9a5ce0b9311e412fb0c53689b334c25619858426
michalwie1 <michaliwieder@gmail.com>
2025-10-15 12:22:30 -0600

    Mail12- change MailList from table to grid element

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,33 +1,18 @@
 const { Link } = ReactRouterDOM
-
 import { MailPreview } from "./MailPreview.jsx"
 
 export function MailList({ mails, onMailClicked, onRemoveMail, onToggleMailRead }) {
-
     return (
-        <table className="mail-list container">
-            <tbody>
-            {mails.map(mail =>
-                <tr className={mail.isRead ? 'read' : ''} key={mail.id} onClick={()=> onMailClicked(mail.id)}>
-                    <MailPreview 
-                        mail = {mail}
-                        onRemoveMail = {onRemoveMail}
-                        onToggleMailRead = {onToggleMailRead} 
-                    />
-                    {/* <section> */}
-                        {/* <button onClick={ev => onRemoveMail(mail.id)}>Delete</button> */}
-
-                        {/* <button onClick={() => onSelectmailId(mail.id)} >Details</button> */}
-        
-                        {/* <button><Link to={`/mail/${mail.id}`}>Details</Link></button> */}
-                        {/* <button><Link to={`/mail/edit/${mail.id}`}>Edit</Link></button> */}
-
-                    {/* </section> */}
-                </tr>
-            )}
-            </tbody>
-        </table>
+        <section className="mail-list">
+            {mails.map(mail => (
+                <MailPreview
+                    key={mail.id}
+                    mail={mail}
+                    onRemoveMail={onRemoveMail}
+                    onToggleMailRead={onToggleMailRead}
+                    onMailClicked={onMailClicked}
+                />
+            ))}
+        </section>
     )
-
-
 }
--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,62 +1,51 @@
-import { LongTxt } from "./LongTxt.jsx"
 import { mailService } from "../services/mail.service.js"
-import deleteIcon from "../../../assets/img/delete.svg"
 
-
-const { useState, useEffect, Fragment } = React
-
-export function MailPreview({ mail, onRemoveMail, onToggleMailRead }) {
+export function MailPreview({ mail, onMailClicked, onRemoveMail, onToggleMailRead }) {
     const { from, subject, sentAt, isRead } = mail
-    const date = formatDate()
-    
-    function formatDate(){
-        return mailService.formatDate(sentAt)
-    }
+    const date = mailService.formatDate(sentAt)
+    const readState = isRead ? 'read' : 'unread'
 
-    // const unreadActionClass = isRead ? '-open' : ''
-    const unreadAction = isRead ? 'unread' : 'read'
-
-    
     return (
+        <section
+            className={`mail-preview ${readState}`}
+            onClick={() => onMailClicked(mail.id)}>
+        
+            <img
+                src="../../../assets/img/checkbox_blank.svg"
+                title="Select"
+                onClick={ev => ev.stopPropagation()}/>
+            
 
-        // <section className="mail-preview">
-        <Fragment>
-            <td>
-                {/* <input type="checkbox" onClick={(ev) => ev.stopPropagation()}></input> */}
-                <img src="../../../assets/img/checkbox_blank.svg" title="Select" onClick={(ev) => ev.stopPropagation()}></img>
+            <img
+                src="../../../assets/img/star.svg"
+                title="Star"
+                onClick={ev => ev.stopPropagation()}/>
+            
 
-            </td>
+            <img
+                src="../../../assets/img/important.svg"
+                title="Mark important"
+                onClick={ev => ev.stopPropagation()}/>
+            
 
-            <td>
-                {/* <i className="fa-regular fa-star"></i> */}
-                <img src="../../../assets/img/star.svg" title="Not starred" onClick={(ev) => ev.stopPropagation()}></img>
-            </td>
-            <td>
-                <img src="../../../assets/img/important.svg" title="Click to teach Gmail this coversation is important" onClick={(ev) => ev.stopPropagation()}></img>
-            </td>
+            <p className="from">{from}</p>
+            <p className="subject">{subject}</p>
 
-            <td>    
-                <p>{from}</p>
-            </td>
-           
-            <td class="subject">    
-                {/* <LongTxt txt={subject}/> */}
-                {subject}
-            </td>
-
-            <td className="date-actions-wrapper">
-                <div className="date">
-                    {date}
+            <div className="date-actions-wrapper" onClick={ev => ev.stopPropagation()}>
+                <div className="date">{date}</div>
+                <div className="actions">
+                    <img
+                        src="../../../assets/img/delete.svg"
+                        title="Delete"
+                        onClick={() => onRemoveMail(mail.id)}/>
+                    
+                    <img
+                        src={`../../../assets/img/${readState}.svg`}
+                        title={`Mark as ${readState}`}
+                        onClick={() => onToggleMailRead(mail.id)}/>
+                    
                 </div>
-
-                <div className="actions" onClick={(ev) => ev.stopPropagation()}>
-                    {/* <i className="fa-solid fa-trash" title="Delete" onClick={() => onRemoveMail(mail.id)}></i> */}
-                    <img src="../../../assets/img/delete.svg" title="Delete" onClick={() => onRemoveMail(mail.id)}></img>
-                    {/* <i className={`fa-solid fa-envelope${unreadActionClass}`} title={`Mark as ${unreadAction}`} onClick={() => onToggleMailRead(mail.id)}></i> */}
-                    <img src={`../../../assets/img/${unreadAction}.svg`} title={`Mark as ${unreadAction}`} onClick={() => onToggleMailRead(mail.id)}></img>
-                </div>
-            </td>   
-        </Fragment>
-
+            </div>
+        </section>
     )
 }
