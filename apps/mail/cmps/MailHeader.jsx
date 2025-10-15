9a176c83d94edd12224720dff626def0a6896386
michalwie1 <michaliwieder@gmail.com>
2025-10-14 22:05:48 -0600

    Mail10- add more page css - logo, layout

---
+++ apps/mail/cmps/MailHeader.jsx
@@ -0,0 +1,13 @@
+import { MailFilter } from "./MailFilter.jsx"
+
+export function MailHeader({ onSetFilterBy, defaultFilter}) {
+
+    return (
+        <section className="mail-header">
+            <img src="../../../assets/img/gmail.png"></img>
+            <h2>MisterMail</h2>
+            <MailFilter onSetFilterBy={onSetFilterBy} defaultFilter={defaultFilter} />
+
+        </section>
+    )
+}
