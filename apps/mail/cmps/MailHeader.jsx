8d16904d6355b53f1cb5b6bebdbc3dba5406a0fe
michalwie1 <michaliwieder@gmail.com>
2025-10-15 10:18:33 -0600

    Add google icons

--- apps/mail/cmps/MailHeader.jsx
+++ apps/mail/cmps/MailHeader.jsx
@@ -4,7 +4,8 @@ export function MailHeader({ onSetFilterBy, defaultFilter}) {
 
     return (
         <section className="mail-header">
-            <img src="../../../assets/img/gmail.png"></img>
+            {/* <img src="../../../assets/img/gmail.png"></img> */}
+            {/* <img src="assets/img/gmail.png"></img> */}
             <h2>MisterMail</h2>
             <MailFilter onSetFilterBy={onSetFilterBy} defaultFilter={defaultFilter} />
 
