7c8282b127ed73506672129997e73001e7e3b9f3
michalwie1 <michaliwieder@gmail.com>
2025-10-20 23:40:52 -0400

    MailDetails first style

--- apps/mail/pages/MailDetails.jsx
+++ apps/mail/pages/MailDetails.jsx
@@ -2,8 +2,9 @@ const { useState, useEffect } = React
 const { useParams, useNavigate, Link } = ReactRouterDOM
 
 
-import { Loader } from "../cmps/Loader.jsx"
 import { mailService } from "../services/mail.service.js"
+import { MailIndex } from "./MailIndex.jsx"
+import { Loader } from "../cmps/Loader.jsx"
 
 export function MailDetails() {
 const [mail, setMail] = useState(null)
@@ -29,11 +30,36 @@ function onBack() {
         navigate('/mail')
     }
 
+// function onRemoveMail(){
+//     MailIndex.
+// }
+
 if (!mail) return <Loader />
 
     return (
         <section className="mail-details">
-            <i className="fa-solid fa-arrow-left" onClick={onBack}></i>
+
+            <div className="actions">
+            <span className= "material-symbols-outlined"
+                    title= "Back to Inbox"
+                    onClick={onBack}
+                        >arrow_back
+            </span>
+
+             <span className= "material-symbols-outlined"
+                    title= "Delete"
+                    // onClick={onRemoveMail}
+                        >delete
+            </span>
+
+            <span 
+                className="material-symbols-outlined" 
+                // title={mailService.capitalizeFirstLetter(readState)}
+                // onClick={(event) => onMailActionToggle(event,mail.id, 'isRead')}
+                >{mail.isRead ? 'drafts' : 'mark_email_unread'}
+            </span>
+
+            </div>
             <h2>{mail.subject}</h2>
             <div>
                 <p>{mail.from}</p>
